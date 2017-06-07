ea07cb12af367677d2439269248aadfce61366f3
miloszszl <miloszszl@interia.pl>
2017-06-07 14:08:32 +0200

    asd

--- my_app/serializers.py
+++ my_app/serializers.py
@@ -132,10 +132,14 @@ class UserSerializer(serializers.ModelSerializer):
                                 redirection_data=pt_data.pop('redirection', None)
 
                                 pt_obj = Page_Test.objects.create(test=t_obj, redirection=None, **pt_data)
-                                if page_data is not None:
+                                if page_data is not None and page_data.get('domain_name',None) is not None and\
+                                        page_data.get('domain_name',None)!="":
                                     host_data=page_data.pop('host', None)
 
-                                    if host_data is not None:
+                                    ph_obj=None
+
+                                    if host_data is not None and host_data.get('domain_name',None) is not None and\
+                                        host_data.get('domain_name',None)!="":
                                         ph_obj=Page_Host.objects.filter(domain_name=host_data.get('domain_name',None))#, ipv4=host_data.get('ipv4',None)
 
                                         if len(ph_obj) <= 0:
